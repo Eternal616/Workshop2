f6bd1019061f589a4beeeea16dec78fb027e72ab
María Jose Vega Petro <mariajosevega72@gmail.com>
2024-02-11 16:35:30 -0500

    agregando la URL base a shop y detalles de productos

--- scripts/detalleProducto.js
+++ scripts/detalleProducto.js
@@ -1,15 +1,23 @@
-import { products } from "../modules/productos.js";
 
+const URL_BASE = "https://backendfalsojsonserver-dev-azcn.2.us-1.fl0.io/";
 
 const idProduct = JSON.parse(localStorage.getItem("idProduct"));
 
-const selectedProduct = products.find((product) => product.id == idProduct);
-console.log(selectedProduct);
+const getProducts = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
 
 const printTipoProducto = (tipoProduct) => {
     let html = "";
     tipoProduct.forEach((element) => {
-    html += `<span>${element}</span>`;
+        html += `<span>${element}</span>`;
     });
 
     return html;
@@ -21,13 +29,12 @@ const printSize = (stock) => {
     const tallas = stock.map((item) => item.talla);
     const tallasDisponibles = [...new Set(tallas)];
     tallasDisponibles.forEach((item) => {
-    let innerhtml = "";
-    const colores = stock.filter((element) => element.talla == item);
-    // console.log(colores)
-    colores.forEach((color) => {
-        innerhtml += `<span class=${item} style="background-color:${color.color}; display:none">${color.color}</span>`;
-    });
-    html += `<button class="tallas">${item}</button>
+        let innerhtml = "";
+        const colores = stock.filter((element) => element.talla === item);
+        colores.forEach((color) => {
+            innerhtml += `<span class="${item}" style="background-color:${color.color}; display:none">${color.color}</span>`;
+        });
+        html += `<button class="tallas">${item}</button>
                 ${innerhtml}
         `;
     });
@@ -35,16 +42,15 @@ const printSize = (stock) => {
     return html;
 };
 
-const showColors = (stock) => {
+const showColors = () => {
     const sizeButtons = document.querySelectorAll(".tallas");
-    console.log(sizeButtons);
     sizeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const colorsButtons = document.querySelectorAll(`.${button.innerText}`);
-        colorsButtons.forEach((btn) => {
-        btn.style.display = btn.style.display == "block" ? "none" : "block";
+        button.addEventListener("click", () => {
+            const colorsButtons = document.querySelectorAll(`.${button.innerText}`);
+            colorsButtons.forEach((btn) => {
+                btn.style.display = btn.style.display === "block" ? "none" : "block";
+            });
         });
-    });
     });
 };
 
@@ -55,16 +61,19 @@ const printDetailsProduct = (product) => {
     h1.innerText = product.nombre;
     main.innerHTML = `
         <figure class="mainImage">
-            <img src=${product.imagen[0]}>
+            <img src="${product.imagen[0]}">
         </figure>
         ${printTipoProducto(product.tipoProduct)}
         <span>${product.precioUnitario}</span>
         ${printSize(product.stock)}
         <span>${product.descripcion}</span>
-
-        
     `;
 };
 
-printDetailsProduct(selectedProduct);
-showColors(selectedProduct.stock);
+document.addEventListener("DOMContentLoaded", async () => {
+    const url = `${URL_BASE}products`;
+    const products = await getProducts(url);
+    const selectedProduct = products.find((product) => product.id === idProduct);
+    printDetailsProduct(selectedProduct);
+    showColors();
+});
--- scripts/sProduct.js
+++ scripts/sProduct.js
@@ -1,6 +1,17 @@
-import { products } from "../modules/productos.js";
+// import { products } from "../modules/productos.js";
 
+const URL_BASE = "https://backendfalsojsonserver-dev-azcn.2.us-1.fl0.io/";
 
+const getproducts = async (url) => {
+    try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+    } catch (error) {
+    console.log(error);
+    return [];
+    }
+};
 
 const containerCards = document.getElementById("containerCardProducts");
 
@@ -8,18 +19,18 @@ const goToDetailsProduct = () => {
     const cards = document.querySelectorAll(".cardProducto");
 
     cards.forEach((card) => {
-        card.addEventListener("click", () => {
-            const idProduct = card.getAttribute("data-id");
-            localStorage.setItem("idProduct", idProduct);
-            location.href = "../pages/detailsProducts.html";
-        });
+    card.addEventListener("click", () => {
+        const idProduct = card.getAttribute("data-id");
+        localStorage.setItem("idProduct", idProduct);
+        location.href = "../pages/detailsProducts.html";
+    });
     });
 };
 
 const imprimirProductos = (container, listadoproduct) => {
     container.innerHTML = "";
-    listadoproduct.forEach(element => {
-        container.innerHTML += `
+    listadoproduct.forEach((element) => {
+    container.innerHTML += `
         <article class="cardProducto" data-id="${element.id}" data-click="card">
             <img src=${element.imagen[0]} alt=${element.nombre} data-click="card">
             <h3 data-click="card">${element.nombre}</h3>
@@ -28,53 +39,55 @@ const imprimirProductos = (container, listadoproduct) => {
         `;
     });
     goToDetailsProduct();
-}
-imprimirProductos(containerCards, products);
+};
 
+document.addEventListener("DOMContentLoaded", async () => {
+    const url = `${URL_BASE}products`;
+    const productos = await getproducts(url);
+    imprimirProductos(containerCards, productos);
+    goToDetailsProduct();
+    });
 
 
 const mostrarTodosLosProductos = () => {
-    imprimirProductos(containerCards, products);
-}
+imprimirProductos(containerCards, products);
+};
 
 const btnMostrarTodos = document.getElementById("mostrarTodos");
 
 btnMostrarTodos.addEventListener("click", mostrarTodosLosProductos);
 
-
 imprimirProductos(containerCards, products);
 
-
 function ordenarAscendente(arreglo) {
-    return arreglo.slice().sort((a, b) => a.precioUnitario - b.precioUnitario);
-    }
+  return arreglo.slice().sort((a, b) => a.precioUnitario - b.precioUnitario);
+}
 
-    function ordenarDescendente(arreglo) {
-    return arreglo.slice().sort((a, b) => b.precioUnitario - a.precioUnitario);
-    }
-    
-    console.log(
-    "El orden del precio de los productos en orden Ascendente es:",
-    ordenarAscendente(products)
-    );
-    console.log(
-    "El orden del precio de los productos en forma Descendente es:",
-    ordenarDescendente(products)
-    );
+function ordenarDescendente(arreglo) {
+  return arreglo.slice().sort((a, b) => b.precioUnitario - a.precioUnitario);
+}
 
+console.log(
+  "El orden del precio de los productos en orden Ascendente es:",
+  ordenarAscendente(products)
+);
+console.log(
+  "El orden del precio de los productos en forma Descendente es:",
+  ordenarDescendente(products)
+);
 
-    const selectOrdenar = document.getElementById("ordenarProductos");
+const selectOrdenar = document.getElementById("ordenarProductos");
 
 // Agregar evento de cambio al select
 selectOrdenar.addEventListener("change", () => {
-    const opcionSeleccionada = selectOrdenar.value;
-    let productosOrdenados;
+  const opcionSeleccionada = selectOrdenar.value;
+  let productosOrdenados;
 
-    if (opcionSeleccionada === "ascendente") {
+  if (opcionSeleccionada === "ascendente") {
     productosOrdenados = ordenarAscendente(products);
-    } else if (opcionSeleccionada === "descendente") {
+  } else if (opcionSeleccionada === "descendente") {
     productosOrdenados = ordenarDescendente(products);
-    }
+  }
 
-    imprimirProductos(containerCards, productosOrdenados);
+  imprimirProductos(containerCards, productosOrdenados);
 });
