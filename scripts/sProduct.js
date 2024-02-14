1bab0edcd73d09f60f81666742b69b3b2ebef2ab
María Jose Vega Petro <mariajosevega72@gmail.com>
2024-02-14 15:45:23 -0500

    Probando si se muestran las imagenes en our products

--- scripts/sProduct.js
+++ scripts/sProduct.js
@@ -32,7 +32,7 @@ const imprimirProductos = (container, listadoproduct) => {
     listadoproduct.forEach((element) => {
     container.innerHTML += `
         <article class="cardProducto" data-id="${element.id}" data-click="card">
-            <img src="..."${element.imagen[0]} alt=${element.nombre} data-click="card">
+            <img src=${element.imagen[0]} alt=${element.nombre} data-click="card">
             <h3 data-click="card">${element.nombre}</h3>
             <span data-click="card">$${element.precioUnitario}</span>
         </article>
